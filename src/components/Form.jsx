b5fae3f9f8bdafab59558320e3a50f0b894e6165
EmilianoAprile <emilianoaprile77@icloud.com>
2024-06-19 15:41:31 +0200

    add > formData with console.log

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,88 +1,126 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 function Form() {
-  // inizializzo gli state per recuperare il value dell'input e l'array nel quale varranno inseriti
-  const [titolo, setTitolo] = useState("");
-  const [lista, setLista] = useState([]);
-  const [isEditing, setIsEditing] = useState(false);
-  const [indexToEdit, setIndexToEdit] = useState(null);
-
-  // sincronizzo il valore del title con l'oggetto event aggiornando lo stato tramite setNewTitle
-  const handleNewTitle = (e) => {
-    setTitolo(e.target.value);
+  const tags = ["Economia", "Arte", "Tecnologia", "Scienza"];
+  const initialData = {
+    title: "",
+    image: "",
+    content: "",
+    category: "",
+    tags: [],
+    published: false,
   };
+  
+  const [formData, setFormData] = useState(initialData);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // quando l'utente clicca su modifica isEditing è true => aggiorno la lista
-    if(isEditing) {
-        // mappo la lista e ritorno l'array modificato controllando se index === indexToEdit sostituendo l'elemento corrente con il nuovo
-        const updatedLista = lista.map((item, index) => index === indexToEdit ? titolo : item);
-        setLista(updatedLista);
-        setIsEditing(false);
-        setIndexToEdit(null);
-    // altrimenti isEditing è false => non è in modalità modifica e aggiungi normalmente il titolo
+    console.log(formData);
+    setFormData(initialData);
+  };
+
+  const handleField = (event) => {
+    const value =
+      event.target.type === "checkbox" ? event.target.checked : event.target.value;
+    if (event.target.name === "tags") {
+
+      const updatedTags = value ? [...formData.tags, event.target.value] : formData.tags.filter((tag) => tag !== event.target.value);
+      setFormData((formData) => ({
+        ...formData,
+        tags: updatedTags,
+      }));
+
+    } else if (event.target.name === "published") {
+      setFormData((formData) => ({
+        ...formData,
+        published: value,
+      }));
+
     } else {
-        setLista((lista) => [titolo, ...lista]);
+      setFormData((formData) => ({
+        ...formData,
+        [event.target.name]: value,
+      }));
     }
-    setTitolo("");
-  };
-
-  const removeTitle = (indexToEliminate) => {
-    setLista((lista) => lista.filter((_, index) => index !== indexToEliminate));
-  };
-
-// funzione che imposta lo stato di isEditing da false a true e recupera l'index del titolo da modificare e aggiorna lo stato dell'index da editare
-  const updateTitle = (indexToEdit) => {
-    setTitolo(lista[indexToEdit]);
-    setIsEditing(true);
-    setIndexToEdit(indexToEdit);
   };
 
   return (
     <>
       <section className="formSection">
         <div className="container">
-          <h2>{isEditing ? "Modifica il Titolo" : "Aggiungi un titolo"}</h2>
+          <h2>Aggiungi un titolo</h2>
           <form onSubmit={handleSubmit}>
             <div className="formWrapper">
               <input
                 type="text"
+                name="title"
                 placeholder="Inserisci un titolo.."
-                value={titolo}
-                onChange={handleNewTitle}
+                value={formData.title}
+                onChange={handleField}
                 required
               />
-              <button className="btn btnAdd">{isEditing ? "Salva Modifica" : "Aggiungi"}</button>
+
+              <input
+                type="text"
+                name="image"
+                placeholder="Scegli un immagine..."
+                value={formData.image}
+                onChange={handleField}
+              />
+
+              <textarea
+                name="content"
+                placeholder="Inserisci il contenuto.."
+                value={formData.content}
+                onChange={handleField}
+                required
+              ></textarea>
+
+              <select
+                name="category"
+                value={formData.category}
+                onChange={handleField}
+                required
+              >
+                <option value="">Seleziona una categoria</option>
+                <option value="Tecnologia">Tecnologia</option>
+                <option value="Scienza">Scienza</option>
+                <option value="Arte">Arte</option>
+                <option value="Economia">Economia</option>
+              </select>
+
+              <div>
+                <p>Tags:</p>
+                {tags.map((tag) => (
+                  <label key={tag}>
+                    <input
+                      type="checkbox"
+                      name="tags"
+                      value={tag}
+                      checked={formData.tags.includes(tag)}
+                      onChange={handleField}
+                    />
+                    {tag}
+                  </label>
+                ))}
+              </div>
+
+              <label htmlFor="published">
+                Pubblicato
+                <input
+                  type="checkbox"
+                  name="published"
+                  id="published"
+                  checked={formData.published}
+                  onChange={handleField}
+                />
+              </label>
+
+              <button className="btn btnAdd" type="submit">
+                Aggiungi
+              </button>
             </div>
           </form>
-          <h2>Titoli</h2>
-          {/* conditional rendering se ci sono titoli renderizza la lista altrimenti il paragrafo */}
-          {lista.length > 0 ? (
-            <ul className="titleList">
-              {lista.map((listItem, index) => (
-                <li className="listItem" key={`title${index}`}>
-                  {listItem}
-                  <div className="buttons">
-                    <button
-                      className="btn btnUpdate"
-                      onClick={() => updateTitle(index)}
-                    >
-                      Modifica
-                    </button>
-                    <button
-                      className="btn btnRemove"
-                      onClick={() => removeTitle(index)}
-                    >
-                      Elimina
-                    </button>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p className="listMessage">Non hai ancora inserito nessun titolo</p>
-          )}
         </div>
       </section>
     </>
